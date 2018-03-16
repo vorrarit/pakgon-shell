30ea591bf4f7609e899a23887651e1fcd159f67c
Vorrarit L <vorrarit@gmail.com>
2018-03-16 08:30:04 +0700

    init

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as TabGroup from 'electron-tabs';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+	private tabGroup: TabGroup;
+	private activeTab: any;
+	public model: {
+		currentAddress: string
+	};
 
-  ngOnInit() {
-  }
+	constructor() { }
+
+	ngOnInit() {
+		this.model = { currentAddress: '' };
+
+		this.tabGroup = new TabGroup();
+		this.tabGroup.on("tab-active", (tab, tabGroup) => {
+			this.model.currentAddress = tab.webview.src;
+		});
+		this.tabGroup.on("tab-added", (tab, tabGroup) => {
+			tab.webview.addEventListener('page-title-updated', (e) => {
+				tab.setTitle(e.title);
+			});
+			tab.webview.addEventListener('update-target-url', (url) => {
+				if (tab == this.tabGroup.getActiveTab()) {
+					this.model.currentAddress = tab.webview.src;
+				}
+			});
+			tab.webview.addEventListener('will-navigate', (url) => {
+				console.log('will navigate', url);
+			});
+			tab.webview.addEventListener('new-window', (e) => {
+				this.tabGroup.addTab({
+					src: e.url,
+					visible: true,
+					active: true
+				});
+			});
+		});
+	
+		this.tabGroup.addTab({
+			title: 'Google',
+			src: 'http://google.com',
+		});
+		this.tabGroup.addTab({
+			title: "Electron",
+			src: "http://electron.atom.io",
+			visible: true,
+			active: true
+		});
+	}
+
+	onSubmit() {
+		this.tabGroup.getActiveTab().webview.loadURL(this.model.currentAddress);
+	}
 
 }
